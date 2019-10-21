4fc17dcf82efd94584047891fd298e1fd18cb862
Ariel <a.o.hyman@gmail.com>
2019-10-21 01:34:40 -0400

    min rotations challenge

---
+++ Level_2/minimumRotations.js
@@ -0,0 +1,13 @@
+/*
+Minimum rotations required to get the same String
+
+Given a string, we need to find the minimum number of rotations required to get the same string. In this case, we will only consider Left rotations.
+
+Example:
+
+Input : s = “geeks”
+Output : 5
+
+Input : s = “aaaa”
+Output :1 
+*/
