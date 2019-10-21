7fddc00c7dc0ab3bd0ba3c465248961126237112
Ariel <a.o.hyman@gmail.com>
2019-10-21 01:29:46 -0400

    cyclical linked list challenge

---
+++ Level_2/Data_Structures/cyclicalLinkedList.js
@@ -0,0 +1,15 @@
+/*
+Given a linked list, determine if it has a cycle in it.
+
+
+ x - x - x - x
+        |   |
+        x - x
+
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+*/
+
+//source: https://leetcode.com/problems/linked-list-cycle/
