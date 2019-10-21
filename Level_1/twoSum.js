17694114f023a8cdcc9a621c7f542bc6fda91911
Ariel <a.o.hyman@gmail.com>
2019-10-21 01:41:38 -0400

    twosum challenge

---
+++ Level_1/twoSum.js
@@ -0,0 +1,13 @@
+/*
+Design a method that takes an array of positive integers and returns the indices of the two distinct integers in the array that sum up to a specific target value.
+
+Notes:
+
+You may assume that each input would have exactly one solution.
+Can you think of at least two solutions/algorithms to solve the problem: one that is space efficient and another that is time efficient?
+Example: Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+
+Add on challenge: Extend your solution so that it returns all pairs of indices that solve the problem.
+*/
