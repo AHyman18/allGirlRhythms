38f670140123232b43af4a0a1da40ec299b38b63
Ariel <a.o.hyman@gmail.com>
2019-10-21 00:44:41 -0400

    rename md to js

---
+++ Level_2/2dArray.js
@@ -0,0 +1,12 @@
+/*
+Write a function that takes in a 2-D array that represents a square matrix and returns the product of the left-to-right diagonal of the matrix. 
+
+For example:
+const matrix = [
+  [2,   3],
+  [4,   5]
+]
+
+Output: 10
+
+*/
