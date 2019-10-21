d03aaae4c14a60be56fbba9d44b3f183b88d19a3
Ariel <a.o.hyman@gmail.com>
2019-10-21 00:40:26 -0400

    rename md to js

--- Level_1/secondMinNode.js
+++ Level_1/secondMinNode.js
@@ -1,3 +1,4 @@
+/*
 Given a non-empty special binary tree consisting of nodes with the non-negative value, where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes, then this node's value is the smaller value among its two sub-nodes. More formally, the property root.val = min(root.left.val, root.right.val) always holds.
 
 Given such a binary tree, you need to output the second minimum value in the set made of all the nodes' value in the whole tree.
@@ -25,5 +26,12 @@ Input:
 
 Output: -1
 Explanation: The smallest value is 2, but there isn't any second smallest value.
+*/
 
-Source: https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
+function BST(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+
+//Source: https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
