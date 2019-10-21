4d607faff6d10c0af5258bb65a4a8bd9bcc2ff2c
Ariel <a.o.hyman@gmail.com>
2019-10-21 00:59:03 -0400

    rangeSumQuery challenge

---
+++ Level_2/rangeSumQuery.js
@@ -0,0 +1,19 @@
+/*
+
+Problem
+
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Note:
+* You may assume that the array does not change.
+* There are many calls to sumRange function. How can you optimize to prevent repeating work?
+
+Example
+
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+
+*/
